3e7cd82ba95407967029ff14788b0d76244f6e24
hardeyphemmy <adegboyeadeyemo0@gamil.com>
2023-08-19 14:55:51 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in lower case
+ */
+
+void print_alphabet(void)
+{
+	char lette;
+
+	for (letter = 'a'; leter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void printt_alpabet_x10(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		ch = 'a';
+		while
+			(ch <= 'z');
+
+		{
+		putchar(ch)
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
