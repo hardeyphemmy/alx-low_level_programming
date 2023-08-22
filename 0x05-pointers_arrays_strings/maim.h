a4b3f1bec9d4c182835f3ef5414f37be1aa862a5
hardeyphemmy <adegboyeadeyemo0@gamil.com>
2023-08-22 03:43:40 -0700

    Pointers, arrarys and strings

---
+++ 0x05-pointers_arrays_strings/maim.h
@@ -0,0 +1,14 @@
+#ifndef MAIM_H
+#define MAIM_H
+
+// Function prototypes
+int add(int a, int b);
+
+void printHello();
+
+int _atoi(char *s);
+
+// Macros
+#define PI 3.14159265
+
+#endif // MAIM_H
