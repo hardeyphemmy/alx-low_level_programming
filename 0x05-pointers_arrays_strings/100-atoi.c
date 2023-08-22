7b9398cd9664c0a898f34f5915c10985facb0692
hardeyphemmy <adegboyeadeyemo0@gamil.com>
2023-08-22 07:31:44 -0700

    Pointers, arraays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,29 +6,36 @@
  */
 int _atoi(char *s)
 {
-	int result = 0;
-	int sign = 1;
-	int i = 0;
+	int i, d, n, len, f, digit;
 
-	while (s[i] == '-' || s[i] == '+')
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-			sign *= -1;
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
 		i++;
-
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > 7))	
-	if (sign == 1)
-
-		return INT_MAX;
-
-	else
-		return INT_MIN;
 	}
-	result = result * 10 + (s[i] - '0');
-	i++;
-	}
-	return result * sign;
+	if (f == 0)
+		return (0);
+	return (n);
 }
+
