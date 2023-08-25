ddbd61465755f1c42891377919dd37271c296744
hardeyphemmy <adegboyeadeyemo0@gamil.com>
2023-08-25 05:27:05 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Retun: Always void
+ */
+
+int main(void)
+{
+	char c;
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z')
+		{
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
