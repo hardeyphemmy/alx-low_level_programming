5b64fb072a3681d1fd9ad9736a63b80774e66556
hardeyphemmy <adegboyeadeyemo0@gamil.com>
2023-09-04 11:00:31 -0700

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mu1 - multiples two integers
+ * @a: - The first inreger
+ * @b: - The second integer
+ * return: Always 0
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
