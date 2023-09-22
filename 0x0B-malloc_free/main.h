70ff2ff0bfd35c534b2d8d3f1e273153fa478819
hardeyphemmy <adegboyeadeyemo0@gamil.com>
2023-09-22 05:36:44 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *create_array(unsigned int size, char c);
+
+char *_strdup(char *str);
+
+int **alloc_grid(int width, int height);
+
+char *str_concat(char *s1, char *s2);
+
+void free_grid(int **grid, int height);
+
+char *argstostr(int ac, char **av);
+
+char **strtow(char *str);
+
+int _putchar(char s);
+
+#endif
