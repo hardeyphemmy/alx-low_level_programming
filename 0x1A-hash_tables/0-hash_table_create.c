f9c257cc6a933d3a3862affd0724e09626c7b987
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-06 08:22:16 +0000

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -39,14 +39,14 @@ typedef struct hash_table_s
  */
 hash_table_t *hash_table_create(unsigned long int size)
 	{
-	hash_table_t *new_table = (hash_table_t *)malloc(sizeof(hash_table_t *));
+	hash_table_t *new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (new_table == NULL)
 	{
 		return (NULL);
 	}
 	new_table->size = size;
-	new_table->array = (hash_node_t **)calloc(size, sizeof(hash_table_t));
+	new_table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t));
 	if (new_table->array == NULL)
 	{
 		free(new_table);
