fa74f8ebd8fccf968e4abd23cc1ccffafce3806c
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-06 11:26:09 +0000

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -68,21 +68,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
-		return (0)
+		return (0);
 	}
 
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node);
-		return (0)
+		return (0);
 	}
 	new_node->value = strdup(value);
 	if (new_node->value == NULL)
 	{
 		free(new_node->key);
 		free(new_node);
-		return (0)
+		return (0);
 	}
 	new_node->next = NULL;
 
