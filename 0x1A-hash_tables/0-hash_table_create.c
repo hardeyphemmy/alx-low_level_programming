a9674c146f2a975261fb04fef3801605e93267c7
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-06 13:40:22 +0000

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "hash_tables.h"
+
 /**
  * hash_node_s - Node of a hash table
  *
@@ -27,7 +28,7 @@ typedef struct hash_table_s
  * Return: A pointer to the newely created or NULL on failure
  */
 hash_table_t *hash_table_create(unsigned long int size)
-	{
+{
 	hash_table_t *new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (new_table == NULL)
