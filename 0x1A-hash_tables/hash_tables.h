754d0ceea877252891e291ac468b53426a2f9a7d
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-06 15:58:04 +0000

    hash header

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -3,7 +3,7 @@
 
 typedef struct hash_table_s hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
-hast_table_set(hash_table_t *ht, const char *key, const char *value);
+int hast_table_set(hash_table_t *ht, const char *key, const char *value);
 
 unsigned long int hash_djb2(const unsigned char *str);
 
