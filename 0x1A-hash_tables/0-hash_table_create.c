e61f4ef538d79ca61c345b314aff95f707ffe3a0
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-06 11:04:07 +0000

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -46,11 +46,57 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	new_table->size = size;
-	new_table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t));
+	new_table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
 	if (new_table->array == NULL)
 	{
 		free(new_table);
 		return (NULL);
 	}
 	return (new_table);
+}
+
+/**
+ * hash_table_set - set the hash table
+ * @ht: The hash to set the key-value pair
+ * @key: The key, string
+ * @value: The corresponding value in char
+ * Return: int success (1) or failure (NULL)
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = hash(key, ht->size);
+	hash_node_t *new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (0)
+	}
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0)
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0)
+	}
+	new_node->next = NULL;
+
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = new_node;
+	}
+	else {
+		hash_node_t *current = ht->array[index];
+		while (current->next != NULL){
+			current = current->next;
+		}
+		current->next = new_node;
+	}
+
+	return(1);
 }
