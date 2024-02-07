3f711d76aee3ec4d466e8dbaa8f92e556e42cbc5
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-07 10:01:06 +0000

    Hash create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,33 +4,19 @@
 #include <string.h>
 
 /**
- * hash_node_s - Node of a hash table
- *
- * @key: The key, string
- * @value: The value corresponding to a key
- * @next: Pointer to the next node of the list
- */
-
-typedef struct hash_node_s
-{
-	char *key;
-	char *value;
-	struct hash_node_s *next;
-} hash_node_t;
-/**
  * hash_table_create - Create a hash table
  * @size: The size of the array
  * Return: A pointer to the newely created or NULL on failure
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
 	if (new_table == NULL)
 	{
 		return (NULL);
 	}
-	*new_table->size = size;
+	new_table->size = size;
 	new_table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
 	if (new_table->array == NULL)
 	{
@@ -38,49 +24,4 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	return (new_table);
-}
-
-/**
- * hash_table_set - set the hash table
- * @ht: The hash to set the key-value pair
- * @key: The key, string
- * @value: The corresponding value in char
- * Return: int success (1) or failure (NULL)
- */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
-	hash_node_t *new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-	{
-		return (0);
-	}
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-	new_node->next = NULL;
-
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = new_node;
-	}
-	else {
-		hash_node_t *current = ht->array[index];
-		while (current->next != NULL){
-			current = current->next;
-		}
-		current->next = new_node;
-	}
-
-	return(1);
 }
