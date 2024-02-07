a57d7f1998745e8da73acb0feebe9398215b41d7
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-07 11:45:37 +0000

    hash table set, init index

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,3 +1,7 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * hash_table_set - set the hash table
  * @ht: The hash to set the key-value pair
@@ -5,39 +9,47 @@
  * @value: The corresponding value in char
  * Return: int success (1) or failure (NULL)
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-        hash_node_t *new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
-        if (new_node == NULL)
-        {
-                return (0);
-        }
+	unsigned long int index;
+	hash_node_t *new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
 
-        new_node->key = strdup(key);
-        if (new_node->key == NULL)
-        {
-                free(new_node);
-                return (0);
-        }
-        new_node->value = strdup(value);
-        if (new_node->value == NULL)
-        {
-                free(new_node->key);
-                free(new_node);
-                return (0);
-        }
-        new_node->next = NULL;
+	if (new_node == NULL)
+	{
+		return (0);
+	}
 
-        if (ht->array[index] == NULL)
-        {
-                ht->array[index] = new_node;
-        }
-        else {
-                hash_node_t *current = ht->array[index];
-                while (current->next != NULL){
-                        current = current->next;
-                }
-                current->next = new_node;
-        }
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+	new_node->next = NULL;
 
-        return(1);
+	index = key_index((const unsigned char *)key, ht->size);
+
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = new_node;
+	}
+	else
+	{
+		hash_node_t *current = ht->array[index];
+
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
+	}
+	return (1);
+}
