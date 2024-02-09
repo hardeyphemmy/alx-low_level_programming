592cdc62aba1526ed0adf910335730d807ca3f9c
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-09 16:22:10 +0000

    printing format of the hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * hash_table_print - print content of the hash table
+ * @ht: the hash table
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			printf("'%s': '%s'", node->key, node->value);
+			if (node->next != NULL)
+				printf(", ");
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
